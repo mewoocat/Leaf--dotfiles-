989b68ac0f4eee9509079fe49089add511a53f44
mewoocat <azure.horizon.4@gmail.com>
2023-03-14 20:14:40 -0500

    Updated volume.sh file path

--- .config/eww/scripts/checkVolume.sh
+++ .config/eww/scripts/checkVolume.sh
@@ -2,5 +2,8 @@
 
 # From: https://stackoverflow.com/questions/34936783/watch-for-volume-changes-in-alsa-pulseaudio
 
-#pactl subscribe | grep --line-buffered "sink" | xargs -n1 ./volume.sh
-pactl subscribe | grep --line-buffered "sink" | while read -r UNUSED_LINE; do /home/ghost/.config/eww/scripts/volume.sh; done
+# Set volume initially 
+~/.config/eww/scripts/volume.sh
+
+# Set volume on change
+pactl subscribe | grep --line-buffered "sink" | while read -r UNUSED_LINE; do ~/.config/eww/scripts/volume.sh; done
